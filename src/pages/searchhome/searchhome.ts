57367c3efa1db99bf77ba93fa58d1a68e27533c1
cherry-cheng <chengyonghui@yizhanit.com>
2018-12-07 17:57:23 +0800

    填充测试数据

--- src/pages/searchhome/searchhome.ts
+++ src/pages/searchhome/searchhome.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
 
 /**
  * Generated class for the SearchhomePage page.
@@ -14,7 +14,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: "searchhome.html"
 })
 export class SearchhomePage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  items: string[] = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    //装载测试数据
+    for(let i=0; i<12; i++) {
+      this.items.push("test" + i);
+    }
+  }
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad SearchPage");
@@ -25,7 +31,24 @@ export class SearchhomePage {
     this.navCtrl.pop();
   }
 
-  onInput(ev: any) {
+  /**
+   *监听input输入框的变化
+   *
+   * @param {*} ev
+   * @memberof SearchhomePage
+   */
+  onInput(ev: any) {}
 
+  /**
+   *监听键盘按钮点击搜索时
+   * 添加手机键盘监听事件(keyup)="showResult()"
+   * @param {*} event
+   * @param {string} keyword
+   * @memberof SearchhomePage
+   */
+  showResult(event: any, keyword: string) {
+    if ("Enter" == event.key) {
+      //function
+    }
   }
 }
