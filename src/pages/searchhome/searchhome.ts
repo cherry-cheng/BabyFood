fe2a4d8928d48acf1ecdb12494f09f58eb50293d
cherry-cheng <chengyonghui@yizhanit.com>
2018-12-07 16:36:53 +0800

    搜索主界面，奇丑无比

--- src/pages/searchhome/searchhome.ts
+++ src/pages/searchhome/searchhome.ts
@@ -10,47 +10,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-searchhome',
-  templateUrl: 'searchhome.html',
+  selector: "page-searchhome",
+  templateUrl: "searchhome.html"
 })
 export class SearchhomePage {
-  searchQuery: string = '';
-  items: string[];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.initializeItems();
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
-  }
-
-  initializeItems() {
-    this.items = [
-      'Amsterdam',
-      'Bogota',
-      'nurdun',
-      'andysh'
-    ];
-  }
-
-  getItems(event: any) {
-    //Reset items back to all of the items
-    this.initializeItems();
-
-    //set val to the value of the searchbar
-    let val = event.target.value;
-
-    //if the value is an empty string dont`t filter the items
-    if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    }
+    console.log("ionViewDidLoad SearchPage");
   }
 
   //返回到主页面
   goBack() {
-    this.navCtrl.pop()
+    this.navCtrl.pop();
   }
 
+  onInput(ev: any) {
+
+  }
 }
