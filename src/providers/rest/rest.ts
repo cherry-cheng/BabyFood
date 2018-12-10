e51d7196c26d0dcae6f6d8dee13148bf3b4e46d6
cherry-cheng <chengyonghui@yizhanit.com>
2018-12-10 18:44:07 +0800

    利用httpClient进行网络请求

---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,71 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { Response } from "@angular/http";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+  constructor(public http: HttpClient) {
+    console.log("Hello RestProvider Provider");
+  }
+
+  private baseUrl = "http://192.168.1.155:9001/v1.0.2/";
+  private getFoodCategory = this.baseUrl + "getfoodcategory";
+
+  /**
+   *全局获取HTTP请求的方法
+   *
+   * @private
+   * @param {string} url
+   * @returns {Observable<string[]>}
+   * @memberof RestProvider
+   */
+  private getUrlReturn(url: string): Observable<string[]> {
+    return this.http
+      .get(url)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  /**
+   *处理接口返回的数据，处理成 json 格式
+   *
+   * @private
+   * @param {Response} res
+   * @returns
+   * @memberof RestProvider
+   */
+  private extractData(res: Response) {
+    let body = res.json();
+    return JSON.parse(body) || {};
+  }
+
+  /**
+   *处理请求中的错误，考虑了各种情况的错误处理并在 console 中显示
+   *
+   * @private
+   * @param {(Response | any)} error
+   * @returns
+   * @memberof RestProvider
+   */
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || "";
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ""} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
