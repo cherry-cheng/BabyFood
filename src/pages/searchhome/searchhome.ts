69d04165ba6cb6213c686f3cd4047a44a8a7035f
cherry-cheng <chengyonghui@yizhanit.com>
2018-12-12 14:59:12 +0800

    搜索结果循环

--- src/pages/searchhome/searchhome.ts
+++ src/pages/searchhome/searchhome.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
 import { Storage } from "@ionic/storage";
 import { NativeStorage } from "@ionic-native/native-storage";
+import { RestProvider } from '../../providers/rest/rest';
+import { e } from '@angular/core/src/render3';
 
 /**
  * Generated class for the SearchhomePage page.
@@ -21,15 +23,28 @@ export class SearchhomePage {
   items: string[] = [];
   isHomeP: boolean = true;
   isSearchHist: boolean = false;
+  isHasResult: boolean = true;
   keywords: any;
   keyword: string;
   searchdata: { searchvalue: string }[] = [];
   inputWord:string;
+
+  searchResultdata: {
+    imgurl: string,
+    title: string,
+    loves: any,
+    label: any,
+    hits: any,
+    type: any
+   }[] = []
+
+
   constructor(
     public navCtrl: NavController,
     public storage: Storage,
     private nativeStorage: NativeStorage,
     public searchProvider: SearchhistoryProvider,
+    public restProvider: RestProvider,
     public navParams: NavParams
   ) {}
 
@@ -86,6 +101,26 @@ export class SearchhomePage {
       .addSearchHis(searchItem)
       .then(data => console.log(data), error => console.log(error));
     this.getSearchHist();
+
+    //调用接口搜索
+    var params = { type: "0", keywords: searchItem};
+    this.restProvider.GET("bbsearch", params, (res, err) => {
+      if (err) {
+        console.log(err);
+        this.isHasResult = false;
+      }
+      if (res) {
+        console.log(res);
+        this.searchResultdata = res.body.food;
+        console.log(this.searchResultdata);
+
+        if (this.searchResultdata != null && this.searchResultdata.length > 0) {
+          this.isHasResult = true;
+        } else {
+          this.isHasResult = false;
+        }
+      }
+    });
   }
 
   /**
